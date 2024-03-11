fc3d1d701587e5054457a8f52e7d4c489c0d016e
Evan Putnam <quorumcode@gmail.com>
2024-03-10 23:13:05 -0700

    Initial commit

---
+++ islands/Navbar.tsx
@@ -0,0 +1,11 @@
+import { Button } from "../components/Button.tsx"
+
+export default function Navbar() {
+    return (
+        <div class="flex mx-auto gap-8 px-4 justify-center">
+            <a href=""> Maps </a>
+            <a href=""> Botany </a>
+            <a href=""> Art </a>
+        </div>
+    );
+}
